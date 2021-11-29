05fcbbd4932b3672bd944f1cabdacb13a86287fc
BelenMaru <bleneee@gmail.com>
2021-11-29 13:38:53 -0500

    refactor vowels

--- katas/kata3-vowels.js
+++ katas/kata3-vowels.js
@@ -1,18 +1,14 @@
-// jshint esversion:6
-
 // Count the number of vowels that appear in a given string
-const numberOfVowels = function(data){
+const numberOfVowels = function (data) {
   const vowels = "aeiou";
-  var count = 0;
-  for (var i = 0; i < data.length; i++ ){
-    if ( vowels.includes(data[i])){
-      count +=1;
+  let count = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (vowels.includes(data[i])) {
+      count += 1;
     }
-
   }
   return count;
 };
-
 
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
